c35a272307b5ff0e989a9c13edf81368ab221303
Michael Smith <selabie68@gmail.com>
2019-06-05 17:47:13 +1000

    Updated check_position.pl to work on SNAX
    Removed SMS alert and replaced it with email alert. Changed strings to use snax binarys

--- check_position.pl
+++ check_position.pl
@@ -1,48 +1,51 @@
 use strict;
-use Net::SMS::TextmagicRest;
-# Author: Eugene Luzgin @ EOS Tribe
-# Install TextMagic:
-# https://www.textmagic.com/docs/api/perl/
-# Crete account, get API key at https://www.textmagic.com/
+use Email::MIME;
+use Email::Sender::Simple qw(sendmail);
+
+# Modified for SNAX by Michael Smith (anarcist)
+# Original Author: Eugene Luzgin @ EOS Tribe
 
 # Change values:
-my $node_dir = "/home/eostribe/Mainnet/producer-node";
-my $producer = "eostribeprod";
-
-my @PHONES = ("CELL-PHONE");
-my $tm = Net::SMS::TextmagicRest->new(
-    username => "USERNAME",
-    token    => "TOKEN",
-);
+my $node_dir = "/opt/snax/data/snaxnode";
+my $producer = 'anarcist';
+my $email_from = '<YOUR EMAIL ADDRESS>';
+my $email_to= '<YOUR EMAIL ADDRESS>';
 
 open LOG, "<$node_dir/bp_position.last";
 my $last_position = <LOG>;
 chomp $last_position;
 close LOG;
 
-
-my @REGDATA = `$node_dir/cleos.sh system listproducers`;
+my @REGDATA = `$node_dir/clisnax.sh system listproducers`;
 my $bp_position = "50+";
 my $counter = 0;
 foreach my $regbp (@REGDATA) {
-   if($regbp=~m/^(\w+)\s+(EOS\w+)\s/) {
+   if($regbp=~m/^(\w+)\s+(SNAX\w+)\s/) {
       $counter++;
       if($1 eq $producer) { $bp_position = $counter };	
    }
 }
 
+if($bp_position != $last_position) {
+   my $message = Email::MIME->create(
+      header_str => [
+         From    => $email_from,
+         To      => $email_to,
+         Subject => 'SNAX Producer Position #'.$bp_position,
+      ],
+      attributes => {
+         encoding => 'quoted-printable',
+         charset  => 'ISO-8859-1',
+      },
+      body_str => "$producer moved #".$last_position." -> #".$bp_position,
+   );
 
-if($bp_position!=$last_position) {
-	my $message = "$producer moved #".$last_position." -> #".$bp_position;
+   sendmail($message);
+
 	open LOG, ">$node_dir/bp_position.last";
 	print LOG $bp_position;
 	close LOG;
 
-# Send SMS Alert if message set:
-	my $result = $tm->send(
-               text    => $message,
-               phones  => \@PHONES,
-        );
-	print "Sent $message SMS[$result->{id}]\n";
+	print "Sent Email!";
 }
 
