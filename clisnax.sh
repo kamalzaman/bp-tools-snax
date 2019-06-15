77c8e5b6ece1cb9059b832c6722964a2c69b0e3c
Michael Smith <selabie68@gmail.com>
2019-06-15 20:21:55 +1000

    Updated claim_snax.pl

--- clisnax.sh
+++ clisnax.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Change path accordingly:
-CLISNAX=/Users/michael/Code/snax/bin-macos/clisnax
-$CLISNAX -u https://cdn.snax.one --wallet-url http://127.0.0.1:8888 "$@"
+CLISNAX=/root/producer/clisnax
+$CLISNAX -u https://cdn.snax.one --wallet-url http://127.0.0.1:8900 "$@"
